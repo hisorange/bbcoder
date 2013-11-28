752fd546387fe6f5cc2d3b5eee172452b11600a1
hisorange <hisoranger@gmail.com>
2013-11-28 02:10:58 +0100

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -1,48 +1,58 @@
 <?php
 return array(
 	// Detailed explanations @ http://milesj.me/code/php/decoda
+	'profile' => array(
+		'default'		=> array(
+			// Parser configurations.
+			'parser'	=> array(
+				'open' => '[',
+				'close' => ']',
+				'locale' => 'hu-hu',
+				'disabled' => false,
+				'shorthandLinks' => false,
+				'xhtmlOutput' => false,
+				'escapeHtml' => true,
+				'strictMode' => true,
+				'maxNewlines' => 3,
+				'lineBreaks' => true,
+				'removeEmpty' => false
+			),
 
-	// Parser configurations.
-	'parser'	=> array(
-		'open' => '[',
-		'close' => ']',
-		'locale' => 'en-us',
-		'disabled' => false,
-		'shorthandLinks' => false,
-		'xhtmlOutput' => false,
-		'escapeHtml' => true,
-		'strictMode' => true,
-		'maxNewlines' => 3,
-		'lineBreaks' => true,
-		'removeEmpty' => false
-	),
+			// Hooks being loaded as default.
+			'hooks'		=> array(
+				'Decoda\Hook\CensorHook' => array(),
+				'Decoda\Hook\ClickableHook' => array(),
+				'Decoda\Hook\CodeHook' => array(),
+				'Decoda\Hook\EmoticonHook' => array('path' => '/assets/imgs/emoticons/', 'extension' => 'png'),
+			),
 
-	// Hooks being loaded as default.
-	'hooks'		=> array(
-		'Decoda\Hook\CensorHook' => array(),
-		'Decoda\Hook\ClickableHook' => array(),
-		'Decoda\Hook\CodeHook' => array(),
-		'Decoda\Hook\EmoticonHook' => array('path' => '/assets/imgs/emoticons/', 'extension' => 'png'),
-	),
+			// Filters being loaded as default.
+			'filters'	=> array(
+				'Decoda\Filter\BlockFilter',
+				'Decoda\Filter\CodeFilter',
+				'Decoda\Filter\DefaultFilter',
+				'Decoda\Filter\EmailFilter',
+				'Decoda\Filter\ImageFilter',
+				'Decoda\Filter\ListFilter',
+				'Decoda\Filter\QuoteFilter',
+				'Decoda\Filter\TableFilter',
+				'Decoda\Filter\TextFilter',
+				'Decoda\Filter\UrlFilter',
+				'Decoda\Filter\VideoFilter',
+			),
 
-	// Filters being loaded as default.
-	'filters'	=> array(
-		'Decoda\Filter\BlockFilter',
-		'Decoda\Filter\CodeFilter',
-		'Decoda\Filter\DefaultFilter',
-		'Decoda\Filter\EmailFilter',
-		'Decoda\Filter\ImageFilter',
-		'Decoda\Filter\ListFilter',
-		'Decoda\Filter\QuoteFilter',
-		'Decoda\Filter\TableFilter',
-		'Decoda\Filter\TextFilter',
-		'Decoda\Filter\UrlFilter',
-		'Decoda\Filter\VideoFilter',
-	),
+			// To only parse specific tags, pass an array of whitelisted tags.
+			'whitelist' => array('b', 'u', 'i', 'br', 'hr', 'img', 'youtube'),
 
-	// To only parse specific tags, pass an array of whitelisted tags.
-	'whitelist' => array('b', 'u', 'i', 'spoiler'),
+			// To not parse specific tags, pass an array of blacklisted tags.
+			'blacklist' => array(),
+		),
+		
+		// Example profile.
+		'comments'	=> array(
+			'whitelist' => array('i', 'b', 'u'),
+		),
+	)
 
-	// To not parse specific tags, pass an array of blacklisted tags.
-	'blacklist' => array(),
+	
 );
